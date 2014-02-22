b9b1e0c4c4268a5b1344baf8ed431b5a4488d884
Daniel Yachcik <dyachcik@gmail.com>
2014-02-21 20:11:22 -0600

    rails new sample_app1 --database postgresql --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp1::Application.config.secret_key_base = 'd7181d644edcf30f88d7e4b9f3d5c9921d8a64585ba169d656fa8a7a423989ef4174763930e78cdd2723131e519266037e1ea054dd00af8fe91ed5c011b55b02'
