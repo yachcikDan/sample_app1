560da4469dba6935fa3f064e6c58e395624eb78e
Daniel Yachcik <dyachcik@gmail.com>
2014-04-26 22:56:59 -0500

    commiting to 9.3 Showing all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,21 +34,23 @@ class UsersController < ApplicationController
   end
   
   private
-  
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
-    
-    #  Before filters
-    
-    def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
-    
-    def correct_user
-          @user = User.find(params[:id])
-          redirect_to(root_url) unless current_user?(@user)
-    end
-    
- end
- 
+
+      def user_params
+        params.require(:user).permit(:name, :email, :password,
+                                     :password_confirmation)
+      end
+
+      # Before filters
+
+      def signed_in_user
+        unless signed_in?
+          store_location
+          redirect_to signin_url, notice: "Please sign in."
+        end
+      end
+
+      def correct_user
+        @user = User.find(params[:id])
+        redirect_to(root_url) unless current_user?(@user)
+        end
+end
