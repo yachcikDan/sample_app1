83fd7461193b5c57c02617c51a594bd7051cc506
Daniel Yachcik <dyachcik@gmail.com>
2014-04-26 19:00:14 -0500

    sign in spec changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -18,11 +19,31 @@ class UsersController < ApplicationController
       end
   end
   
+  def edit
+      @user = User.find(params[:id])
+  end
+  
+  def update
+      @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+        flash[:success] = "Profile updated"
+        redirect_to @user
+      else
+        render 'edit'
+      end
+  end
+  
   private
   
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    #  Before filters
+    
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
     
  end
  
