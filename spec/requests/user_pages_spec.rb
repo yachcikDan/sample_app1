c13bf2f153a12b1d89cc6713e658ab564dad2be1
Daniel Yachcik <dyachcik@gmail.com>
2014-04-28 19:02:24 -0500

    finish user edit, update, index, and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,6 +1,55 @@
 require 'spec_helper'
 
 describe "User pages" do
+  
+  subject { page }
+  
+  describe "index" do
+    
+      let(:user) { FactoryGirl.create(:user) }
+      
+      before do
+        sign_in user
+        visit users_path
+      end
+
+      it { should have_title('All users') }
+      it { should have_content('All users') }
+
+      describe "pagination" do
+
+        before(:all) { 30.times { FactoryGirl.create(:user) } }
+        after(:all)  { User.delete_all }
+
+        it { should have_selector('div.pagination') }
+
+        it "should list each user" do
+          User.paginate(page: 1).each do |user|
+            expect(page).to have_selector('li', text: user.name)
+          end
+        end
+      end
+      
+      describe "delete links" do
+         it { should_not have_link('delete') }
+         
+         describe "as an admin user" do
+           let(:admin) { FactoryGirl.create(:admin) }
+           before do
+             sign_in admin
+             visit users_path
+           end
+           it { should have_link('delete', href: user_path(User.first)) }
+           it "should be able to delete another user" do
+             expect do
+               click_link('delete', match: :first)
+             end.to change(User, :count).by(-1)
+           end
+           it { should_not have_link('delete', href: user_path(admin)) }
+         end
+      end
+  end
+    
 
   subject { page }
 
@@ -30,6 +79,7 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+  
       describe "after submission" do
         before { click_button submit }
           it { should have_content('error') } 
@@ -97,3 +147,4 @@ describe "User pages" do
     end
   end
 end
+
