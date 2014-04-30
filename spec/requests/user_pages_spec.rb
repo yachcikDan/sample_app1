b0b034df513cd576d1211ae0c8d4a6bcf6d971b2
Daniel Yachcik <dyachcik@gmail.com>
2014-04-30 04:19:05 -0500

    chapter 10 up to 10.3.4

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -62,10 +62,19 @@ describe "User pages" do
   
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+    
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
     it { should have_title(user.name) }
+    
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
   
   #Sign-up page tests
@@ -122,18 +131,6 @@ describe "User pages" do
       visit edit_user_path(user)
     end
     
-    describe "forbidden attributes" do
-      let(:params) do
-        { user: { admin: true, password: user.password,
-                      password_confirmation: user.password } }
-      end
-      before do
-        sign_in user, no_capybara: true
-        patch user_path(user), params
-      end
-      specify { expect(user.reload).not_to be_admin }
-    end
-    
     describe "page" do
       it { should have_content("Update your profile") }
       it { should have_title("Edit user") }
@@ -141,6 +138,12 @@ describe "User pages" do
     end
     
     describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
       let(:new_name) { "New Name" }
       let(:new_email) { "new@example.com" }
       before do
@@ -156,6 +159,18 @@ describe "User pages" do
       it { should have_link('Sign out', href: signout_path) }
       specify { expect(user.reload.name).to eq new_name }
       specify { expect(user.reload.email).to eq new_email }
+    end
+    
+    describe "forbidden attributes" do
+      let(:params) do
+        { user: { admin: true, password: user.password,
+                      password_confirmation: user.password } }
+      end
+      before do
+        sign_in user, no_capybara: true
+        patch user_path(user), params
+      end
+      specify { expect(user.reload).not_to be_admin }
     end
   end
 end
